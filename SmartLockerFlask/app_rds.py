de3db5f6a272bd84ce424882408df8d655e34e12
KYEONGJUN LEE <kj10009@naver.com>
2025-04-22 16:39:59 +0900

    Fixed and Added New page

--- SmartLockerFlask/app_rds.py
+++ SmartLockerFlask/app_rds.py
@@ -23,16 +23,18 @@ def register():
         address = request.form['address']
         phone = request.form['phone']
 
+        role = '관리자' if name.strip() == '관리자' else '사용자'
+
         invoice_code = ''.join(random.choices(string.digits, k=12))  # 송장번호만 생성
 
         try:
             with conn.cursor() as cursor:
                 # 1. 사용자 등록
                 sql_user = """
-                    INSERT INTO users (name, address, phone)
-                    VALUES (%s, %s, %s)
+                    INSERT INTO users (name, address, phone, role)
+                    VALUES (%s, %s, %s, %s)
                 """
-                cursor.execute(sql_user, (name, address, phone))
+                cursor.execute(sql_user, (name, address, phone,role))
                 receiver_id = cursor.lastrowid
 
                 sql_delivery = """
@@ -83,6 +85,66 @@ def delete_user(user_id):
 @app.route('/camera')
 def camera():
     return render_template('camera.html')
+
+@app.route('/admin_register', methods=['GET', 'POST'])
+def admin_register():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone'] 
+
+        with conn.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM users WHERE name=%s AND role ='관리자' AND phone=%s",
+                (name, phone)
+            )
+            admin = cursor.fetchone()
+
+        if admin:
+            return redirect('/admin_home')
+        else:
+            # 인증 실패 시 다시 등록 페이지로
+            return redirect('/admin_register')
+
+    return render_template('admin_register.html', failed=True)
+
+@app.route('/user_register', methods=['GET', 'POST'])
+def user_register():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone'] 
+
+        with conn.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM users WHERE name=%s AND role ='사용자' AND phone=%s",
+                (name, phone)
+            )
+            user = cursor.fetchone()
+
+        if user:
+            return redirect('/user_home')
+        else:
+            # 인증 실패 시 다시 등록 페이지로
+            return render_template('user_register.html', failed=True)
+
+    return render_template('user_register.html', failed=True)
+
+
+@app.route('/admin_home')
+def admin_home():
+    return render_template('admin_home.html')
+
+@app.route('/user_home')
+def user_home():
+    return render_template('user_home.html')
+
+@app.route('/input_password')
+def inupt_password():
+    return render_template('input_password.html')
+
+@app.route('/select_locker')
+def inupt_paselect_lockerssword():
+    return render_template('select_locker.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
